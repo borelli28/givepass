2134e684072d079859b25a7bba9668f4abc38e91
Armando <borelliarmando12@gmail.com>
2022-07-11 20:54:31 -0500

    Added file encryption lib

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import pyAesCrypt
 
 
 # ask user for master key, then use master key to decrypt password file
@@ -8,13 +8,22 @@ print(f'master key: {master_key}')
 # give password or enter password
 give_or_enter = input('(a)give me password || (d)take password: ')
 print(f'user wants to: {give_or_enter}')
+
+# open and decrypt file
+
 if give_or_enter == 'a':
-    passwd = open('passwd.txt', 'r')
-    print(passwd.read())
-
-elif give_or_enter == 'd':
-    passwd = open('passwd.txt', 'a')
-    passwd.writelines('that\n')
-
+    d = {}
+    # convert passwd.txt data into a dictionary
+    with open('passwd.txt', 'r') as passwd:
+        for line in passwd:
+           (key, val) = line.split()
+           d[key] = val
+        print(d)
 
 # ask user for account name, then search the dict with the key(account name) and return the value(password)
+elif give_or_enter == 'd':
+    with open('passwd.txt', 'a') as passwd:
+        passwd.writelines('four there\n')
+
+else:
+    print('wrong! try again bozo')
