084b7e654cf2f00a44159ea779bd9c4a46a45cf1
Armando <borelliarmando12@gmail.com>
2022-07-12 08:15:18 -0500

    Added code to delete passwd.txt after creating encrypted file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pyAesCrypt
-
+import os
 
 # ask user for master key, then use master key to decrypt password file
 master_key = input('Enter master key: ')
@@ -10,13 +10,19 @@ give_or_take = input('(a)give me password || (d)take password: ')
 print(f'user wants to: {give_or_take}')
 
 # TODO: Need to encrypt and decrypt file(https://pypi.org/project/pyAesCrypt/)
-# TODO: If passwd.txt does not exist, create and encrypt file
 
 def create_file(key):
     with open('passwd.txt', 'a') as passwd:
         # encrypt file
         pyAesCrypt.encryptFile("passwd.txt", "passwd.txt.aes", master_key)
-        print(f'passwd encrypted: {passwd}')
+        print(f'encrypted passwd created: {passwd}')
+
+    # delete passwd.txt file since the file is a temp file
+    if os.path.exists("passwd.txt"):
+      os.remove("passwd.txt")
+      print('passwd.txt removed')
+    else:
+      print("The file does not exist")
 
 # if passwd file does not exist create one
 try:
@@ -26,8 +32,13 @@ except:
     print('creating passwd file')
     create_file(master_key)
 
+# # read file
 # if give_or_take == 'a':
 #     d = {}
+#
+#     # decrypt file
+#     pyAesCrypt.decryptFile("passwd.txt.aes", "passwd.txt", master_key)
+#
 #     # convert passwd.txt data into a dictionary
 #     with open('passwd.txt', 'r') as passwd:
 #         for line in passwd:
@@ -35,16 +46,26 @@ except:
 #            d[key] = val
 #         print(d)
 #
-#     # # decrypt file
-#     # pyAesCrypt.decryptFile("passwd.txt.aes", "passwd.txt", master_key)
-#     #
-#     # # encrypt file
-#     # pyAesCrypt.encryptFile("passwd.txt", "passwd.txt.aes", master_key)
+#     # encrypt file
+#     pyAesCrypt.encryptFile("passwd.txt", "passwd.txt.aes", master_key)
 #
-# # ask user for account name, then search the dict with the key(account name) and return the value(password)
+# # write(append) to file
 # elif give_or_take == 'd':
+#
+#     # decrypt file
+#     pyAesCrypt.decryptFile("passwd.txt.aes", "passwd.txt", master_key)
+#
 #     with open('passwd.txt', 'a') as passwd:
 #         passwd.writelines('four there\n')
 #
+#     # encrypt file
+#     pyAesCrypt.encryptFile("passwd.txt", "passwd.txt.aes", master_key)
+#
 # else:
 #     print('wrong! try again bozo')
+#
+# def read_encrypted_file(key):
+#     print('inside read_encrypted_file()')
+#
+#     # decrypt file
+#     pyAesCrypt.decryptFile("passwd.txt.aes", "passwd.txt", master_key)
